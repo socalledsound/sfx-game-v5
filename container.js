6ad0f52398ab0e3577ef2fde9f2283b36dcc2896
chris kubick <lrs6000@gmail.com>
2017-06-28 22:16:13 -0700

    more arrows and playbutton

--- container.js
+++ container.js
@@ -1,4 +1,4 @@
-var Container = function(x,y,width,height,color,numCells,sound) {
+var Container = function(x,y,width,height,color,numCells,sounds) {
 	this._x = x;
 	this._y = y;
 	this._width = width;
@@ -7,8 +7,13 @@ var Container = function(x,y,width,height,color,numCells,sound) {
 	this.numCells = numCells;
 	this.draggable = false;
 	this.cells=[];
+	this.cellColor = [2,12,51];
+	this.interfaceColor = [200,200,200];
 	this.currentColumn = 0;
-	this.sound = new Howl({src:['audio/2017clapOS-DG3__1.mp3']});
+	this.sounds = sounds;
+	this.upperTriangles;
+	this.lowerTriangles;
+	// this.sound = new Howl({src:['audio/2017clapOS-DG3__1.mp3']});
 
 	this.initCells = function() {
 			for ( var i=0; i<this.numCells; i++) {
@@ -16,21 +21,61 @@ var Container = function(x,y,width,height,color,numCells,sound) {
 				var y= this._y + ((this._height/this.numCells)*i);
 				var width = this._width * 1.0;
 				var height = this._height * 0.25;
+				var keys = Object.keys(this.sounds);
+				// var cellSounds = this.sounds;
+				var cellSound = this.sounds[keys[i]];
+				// console.log(cellSound);
 				// var sound = new Howl({src:[this.sound] });
-				this.cells[i] = new Cell(x, y, width, height,sound);
+				this.cells[i] = new Cell(x, y, width, height, this.cellColor, cellSound);
 				// console.log(this.cells[i]);
 			}
 	},
 
-	this.playSound = function () {
-
-		this.sound.play();
+	this.initContainerTriangles = function() {
+			this.initUpperTriangles();
+			this.initLowerTriangles();
+			
 	},
+
+
+	this.initUpperTriangles = function() {
+			
+				var a = this._x + 20;
+				var b = this._y - 20;
+				var c = this._x + 50;
+				var d = this._y - 40;
+				var e = this._x + 80;
+				var f = this._y  -20;
+				var fill = this.interfaceColor;
+				var stroke = this.interfaceColor;	
+
+				this.upperTriangles =  new Triangle(a,b,c,d,e,f,fill,stroke);
+				// console.log(this.upperTriangles.a);
+
+
+			
+	},
+
+		this.initLowerTriangles = function() {
+			
+				var a = this._x + 20;
+				var b = this._y + 340;
+				var c = this._x + 50;
+				var d = this._y + 360;
+				var e = this._x + 80;
+				var f = this._y + 340;
+				var fill = this.interfaceColor;
+				var stroke = this.interfaceColor;	
+
+				this.lowerTriangles =  new Triangle(a,b,c,d,e,f,fill,stroke);
+				 console.log(this.lowerTriangles.b);			
+	},
+
 
 	this.moveCells = function() {
 		var that = this;
-			console.log(this._y);
-			console.log(that._y);
+			// console.log(this._y);
+			// console.log(that._y);
 			that.cells.forEach(function(cell,index){
 				// console.log(index);
 				cell.x= that._x + (that._width/20);
@@ -39,23 +84,36 @@ var Container = function(x,y,width,height,color,numCells,sound) {
 				cell.height = that._height * 0.25;
 				console.log(cell.y);
 				})
-			
-
-			// that.settleCellsInGrid();
-			// that.display();
-
-
-	
 	},
 
-	this.checkCellVertical = function() {
+	this.moveTriangles = function() {
+		this.upperTriangles.a = this._x + 20;
+		this.upperTriangles.b = this._y - 20;
+		this.upperTriangles.c = this._x + 50;
+		this.upperTriangles.d = this._y - 40;
+		this.upperTriangles.e =  this._x + 80;
+		this.upperTriangles.f =  this._y  -20;
+
+		this.lowerTriangles.a = this._x + 20;
+		this.lowerTriangles.b = this._y + 340;
+		this.lowerTriangles.c = this._x + 50;
+		this.lowerTriangles.d = this._y + 360;
+		this.lowerTriangles.e = this._x + 80;
+		this.lowerTriangles.f = this._y + 340;
+
+
+	},
+
+
+	this.checkMeridian = function() {
 		var that = this;
 		this.cells.forEach(function(cell,index){
 			if(cell.y >200 && cell.y < 300 && !cell.playing) {
-				cell.cellColor = [200,0,0];
+				// cell.cellColor = [200,0,0];
+				cell.highlight();
 				// console.log(cell.sound);
 				cell.playing = true;
-				 that.playSound();
+				  cell.sound.play();
 				 // console.log(cell.cellPlayingFalse()); 	
 
 				   setTimeout(cell.cellPlayingFalse.bind(cell),1000);
@@ -64,12 +122,37 @@ var Container = function(x,y,width,height,color,numCells,sound) {
 
 	},
 
+	this.checkCellsForClick = function(){
+		var that = this;
+			this.cells.forEach(function(cell,index){
+				
+
+				if( mouseX > cell.x && mouseX < cell.x +80 && mouseY > cell.y && mouseY < cell.y +80) {
+						cell.highlight();
+						// that.display();
+						setTimeout(cell.resetColor.bind(cell),100);
+						//setTimeout(that.display,100);
+						// if (!playing) {
+						cell.clicked = true;
+					//}
+				}	
+
+			})
+
+	},
+
 
 	this.display = function() {
-		var that = this;
-		fill(this._color);
-		rect(this._x, this._y, this._width, this._height);
-		// console.log(this.cells);
+		// var that = this;
+		// fill(this._color);
+		// rect(this._x, this._y, this._width, this._height);
+	// console.log(this.cells);
+			fill(this.interfaceColor);
+			stroke(this.interfaceColor);
+			triangle(this.upperTriangles.a,this.upperTriangles.b,this.upperTriangles.c,this.upperTriangles.d,this.upperTriangles.e,this.upperTriangles.f);
+			triangle(this.lowerTriangles.a,this.lowerTriangles.b,this.lowerTriangles.c,this.lowerTriangles.d,this.lowerTriangles.e,this.lowerTriangles.f);
+
+
 			this.cells.forEach(function(cell,index){
 				// console.log(cell.cellColor);
 				// console.log(index);
@@ -93,10 +176,33 @@ var Container = function(x,y,width,height,color,numCells,sound) {
 		}
 	},
 
+
+	
 	this.checkRect = function() {
 		
 		return (mouseX > this._x && mouseX < this._x + this._width && mouseY > this._y && mouseY < this._y + this._height)		
 	},
+
+	this.updateClick = function() {
+		var that = this;
+		that.cells.forEach(function(cell,index){
+			if(cell.clicked && !cell.playing) {
+			 	cell.sound.play();
+				cell.playing= true;
+			}
+			else {
+				cell.sound.stop();
+				cell.playing = false;
+				cell.clicked = false;
+			}
+			fill(cell.cellColor);
+			strokeWeight(0.5);
+			rect(cell.x-5,cell.y,cell.width,cell.height);
+
+		})
+	},
+
+
 
 	this.unClick = function() {
 		this.draggable = false;
@@ -134,55 +240,121 @@ var Container = function(x,y,width,height,color,numCells,sound) {
 
 	this.checkPlayhead = function(x) {
 		var currentColumn;
-		if (x <40) { currentColumn = 5};
-		if (x > 30 && x < 145) {currentColumn = 0};
-		if (x >140 && x < 245) {currentColumn = 1};
-		if (x >240 && x < 365) {currentColumn = 2};
-		if (x >360 && x < 450) {currentColumn = 3};
-		if (x >450 && x < 555) {currentColumn = 4};
-		if (x >530) {currentColumn = 5};
+		// if (x <40) { currentColumn = 5};
+		// if (x > 30 && x < 145) {currentColumn = 0};
+		// if (x >140 && x < 245) {currentColumn = 1};
+		// if (x >240 && x < 365) {currentColumn = 2};
+		// if (x >360 && x < 450) {currentColumn = 3};
+		// if (x >450 && x < 555) {currentColumn = 4};
+		// if (x >530) {currentColumn = 5};
 		 // console.log(currentColumn);
+		if (x <140) { currentColumn = 5};
+		if (x > 130 && x < 245) {currentColumn = 0};
+		if (x >240 && x < 345) {currentColumn = 1};
+		if (x >340 && x < 465) {currentColumn = 2};
+		if (x >460 && x < 550) {currentColumn = 3};
+		if (x >550 && x < 655) {currentColumn = 4};
+		if (x >630) {currentColumn = 5};
+
 
 		return currentColumn;
 		
 	},
 
-	this.makeRed = function() {
+	this.startColor = function() {
 
 		this.cells.forEach(function(cell,index){
 		// this.checkRedLine();	
-		cell.cellColor = [200,0,0];
+		cell.cellColor = [2,12,51];
+		cell.sound.stop();
 		});
-	},
-
-	this.makeWhite = function() {
-
-		this.cells.forEach(function(cell,index){
-		// this.checkRedLine();	
-		cell.cellColor = [200,200,200];
-		});
-	},
-
-	checkRedLine = function() {
-		this.cells.forEach(function(cell,index){
-			if (this.cell) {
-
-			}
-		})	
 	}
+
+	// this.highlight = function() {
+
+	// 	this.cells.forEach(function(cell,index){
+	// 	// this.checkRedLine();	
+	// 	cell.cellColor = [23,225,239];
+	// 	});
+	// },
+
+	// this.resetColor = function() {
+
+
+	// },
+
+
+	// this.makeRed = function() {
+
+	// 	this.cells.forEach(function(cell,index){
+	// 	// this.checkRedLine();	
+	// 	cell.cellColor = [200,0,0];
+	// 	});
+	// },
+
+	// this.makeWhite = function() {
+
+	// 	this.cells.forEach(function(cell,index){
+	// 	// this.checkRedLine();	
+	// 	cell.cellColor = [200,200,200];
+	// 	});
+	// },
+
+	// checkRedLine = function() {
+	// 	this.cells.forEach(function(cell,index){
+	// 		if (this.cell) {
+
+	// 		}
+	// 	})	
+	// }
 
 
 }
 
+var Interface = function() {
+	this.playButton;
+	this.playButtonClicked = false;
+	this.playButtonPaused = true;
+	
 
-var Cell = function(x, y, width, height,sound) {
+	this.initPlayButton = function() {
+			console.log(height);
+			this.playButton = new PlayButton( 360,700,80 ,80 );
+
+	},
+
+	this.displayInterface = function() {
+
+				strokeWeight(1);
+			stroke(interfaceColor);
+			fill(background_color);
+			rect(this.playButton.x,this.playButton.y,this.playButton.width,this.playButton.height);
+			fill(255);
+			triangle(this.playButton.x+30,this.playButton.y+20,this.playButton.x+30,this.playButton.y+60,this.playButton.x+60,this.playButton.y+40);
+
+
+	},
+
+	this.checkPlayButton = function() {
+				if(checkRect(mouseX, mouseY,this.playButton.x,this.playButton.y,this.playButton.width,this.playButton.height)) {
+				this.playButtonClicked = true;
+				 console.log(this.playButtonClicked);
+		}
+	}
+
+
+
+};
+
+var Cell = function(x, y, width, height, color, sound) {
 	this.x 			= 	x;
 	this.y 			= 	y;
 	this.width 		=	width;
 	this.height 	= 	height;
-	this.cellColor 	=	[200,200,200];
-	this.sound		=	sound;
+	this.cellColor 	=	color;
+	this.sound		=	new Howl({src:[sound]});
 	this.playing	=	false;
+	this.clicked 	=	false;
 	// console.log(this.width);
 	// this.display = function() {
 	// 	fill(this._color);
@@ -191,10 +363,46 @@ var Cell = function(x, y, width, height,sound) {
 	this.cellPlayingFalse = function() {
 		console.log("see it");
 		if(this.playing = true) { this.playing =false};
+	},
+
+	this.highlight = function() {
+
+		this.cellColor = [23,225,239];
+
+	},
+
+	this.resetColor = function() {
+		console.log("see reset");
+		this.cellColor = [2,12,51];
+
 	}	
+
 
 }
 
+var PlayButton = function(x,y,width,height) {
+	this.x 			= 	x;
+	this.y 			= 	y;
+	this.width 		=	width;
+	this.height 	= 	height;
+	this.paused 	= 	true;
+}
+
+var Triangle = function(a,b,c,d,e,f,fillColor,strokeColor) {
+			this.a = a;
+			this.b = b;
+			this.c = c;
+			this.d = d;
+			this.e = e;
+			this.f = f;
+			this.fillColor=fillColor;
+			this.strokeColor = strokeColor;
+
+				fill(this.fillColor);
+				stroke(this.strokeColor);
+				triangle(this.a,this.b,this.c,this.d,this.e,this.f);
+
+};
 
 var CellRow = function(options) {
 	this._x = options.x;
@@ -226,3 +434,5 @@ var CellRow = function(options) {
 
 
 }
+
+
--- sketch.js
+++ sketch.js
@@ -18,38 +18,102 @@
 // 	numSquares : 4
 
 // })
-var containers = []
-var width=620;
-var height=600;
+var sounds = {
+	containers: [
+	{
+		sound_A1 : "sounds/zippers/canvas-suitcase-zipper/cszipper1.ogg",
+		sound_A2 : "sounds/highFairy/highFairy1.ogg",
+		sound_A3 : "sounds/eerieSpace/eerieSpace1.ogg",
+		sound_A4 : "sounds/claps/2017clapOS-DG3__4.mp3"
+	},
+	{
 
+		sound_B1 :  "sounds/zippers/canvas-suitcase-zipper/cszipper2.ogg",
+		sound_B2 : "sounds/highFairy/highFairy1.ogg",
+		sound_A3 : "sounds/eerieSpace/eerieSpace2.ogg",
+		sound_B4 : "sounds/claps/2017clapOS-DG3__8.mp3"
+	},
+	{
+		sound_C1 :  "sounds/zippers/canvas-suitcase-zipper/cszipper3.ogg",
+		sound_C2 : "sounds/highFairy/highFairy1.ogg",
+		sound_A3 : "sounds/eerieSpace/eerieSpace3.ogg",
+		sound_C4 : "sounds/claps/2017clapOS-DG3__12.mp3"		
+	},
+	{
+		sound_D1 :  "sounds/zippers/canvas-suitcase-zipper/cszipper4.ogg",
+		sound_D2 : "sounds/highFairy/highFairy1.ogg",
+		sound_A3 : "sounds/eerieSpace/eerieSpace4.ogg",
+		sound_D4 : "sounds/claps/2017clapOS-DG3__16.mp3"
+	},
+	{
+		sound_E1 :  "sounds/zippers/canvas-suitcase-zipper/cszipper5.ogg",
+		sound_E2 : "sounds/highFairy/highFairy1.ogg",
+		sound_A3 : "sounds/eerieSpace/eerieSpace5.ogg",
+		sound_E4 : "sounds/claps/2017clapOS-DG3__16.mp3"
+	}
+	]
+
+};
+
+
+
+
+
+var containers = []
+var width=800;
+var height=800;
+var playMeridian = height/2-100;
+var xStart = 150;
+var numContainers =5;
+var boxHeight = 80;
+var interface;
+
+var background_color = [11,41,89];
+var background_color = [61,39,17];
+var background_color = [51,52,58];
+var interfaceColor = [200,200,200];
+var vertColor = [200,200,200,80]
 var playHead_x = 10;
-var playHeadPaused = false;
+var playHeadPaused = true;
 var playHeadColor = [40,40,80,100];
+var playHeadColor = [239,228,60,100];
 var soundPath = "audio/2017clapOS-DG3__1.mp3";
 
 function setup() {
 	 
 	//background(0);
-	createCanvas(620,600);
-	background(30,100,100);
-	strokeWeight(10);
-	stroke('red');
-	line(0,height/2,width,height/2);
+	createCanvas(800,700);
+	// background(30,100,100);
+	background(background_color);
+	strokeWeight(boxHeight);
+	stroke(vertColor);
+	line(0,playMeridian,width, playMeridian);
+
 	strokeWeight(3);
+	line(170,80,200,60);
+	line(200,60,230,80);
+	triangle(170,80,200,60,230,80);
+
+
+
 	stroke(30,100,100);
-	for (var i = 0; i<5;i++) {
-		containers[i] = new Container(50+(i*100),100,100,320,[30,100,100],4,soundPath);
+	for (var i = 0; i < numContainers; i++) {
+		containers[i] = new Container(xStart+(i*100),100,100,320,[30,100,100],4,sounds.containers[i]);
 	}
 
 	// container1 = new Container(100,100,100,500,);
 	containers.forEach(function(container){
 		container.initCells();
+		container.initContainerTriangles();
 		container.display();
 	});
-	// cell1.display();
+
+	interface = new Interface();
+	interface.initPlayButton();
+	interface.displayInterface();
 
 
-setInterval(movePlayhead,50);
+//setInterval(movePlayhead,50);
 
 }
 
@@ -60,16 +124,54 @@ function draw() {
 }
 
 
-function mouseClicked() {
-	// containers[0].playSound();
-}
+
+ function mouseClicked() {
+	
+ 	// interface.checkPlayButton();
+
+// 	if(playHeadPaused && interface.playButtonClicked) {
+// 		playHeadPaused = false;
+// 		setInterval(movePlayhead,50);
+// 	}
+
+// 	// else if(playHeadPaused) {
+// 	// 			containers.forEach(function(container){
+// 	// 		container.checkCellsForClick();		
+// 	// 		})
+
+// 	// 	containers.forEach(function(container){
+		
+// 	// 		container.updateClick();		
+		
+// 	// 		})
+// 	// }
+
+// 	else if (!playHeadPaused && interface.playButtonClicked) {
+// 		 playHeadPaused = true;	
+// 	}
+
+
+	if(playHeadPaused) {
+				containers.forEach(function(container){
+			container.checkCellsForClick();		
+			})
+
+		containers.forEach(function(container){
+		
+			container.updateClick();		
+		
+			})
+	}
+
+
+ }
 
 function mouseDragged() {
-playHeadPaused = true;	
-background(30,100,100);
-	strokeWeight(10);
-	stroke('red');
-	line(0,height/2,width,height/2);
+// playHeadPaused = true;	
+background(background_color);
+	strokeWeight(boxHeight);
+	stroke(vertColor);
+	line(0,playMeridian,width,playMeridian);
 	strokeWeight(3);
 	stroke(30,100,100);
 
@@ -78,7 +180,8 @@ containers.forEach(function(container) {
 		if(container.draggable) {
 			console.log("trig movecells");
 			container.move();
-		container.moveCells();
+			container.moveCells();
+			container.moveTriangles();
 		}
 	})
 
@@ -93,7 +196,7 @@ containers.forEach(function(container) {
 }
 
 function mouseReleased() {
-	playHeadPaused = false;
+	//playHeadPaused = false;
 	containers.forEach(function(container) {
 		container.draggable = false;
 	})
@@ -106,12 +209,12 @@ var activeColumn;
 
 	if( playHead_x < width) {playHead_x = playHead_x +10} else {playHead_x = 10};
 
-	background(30,100,100);
-	strokeWeight(10);
-	stroke('red');
-	line(0,height/2,width,height/2);
-
-	stroke(30,100,100);
+	background(background_color);
+	strokeWeight(boxHeight);
+	stroke(vertColor);
+	line(0,playMeridian,width,playMeridian);
+	strokeWeight(3);
+	 stroke(30,100,100);
 
 		containers.forEach(function(container) {	
 		 activeColumn = container.checkPlayhead(playHead_x);
@@ -122,11 +225,11 @@ var activeColumn;
 
 	containers.forEach(function(container,index) {
 	if(index === activeColumn ) {
-		
-		container.checkCellVertical();
+
+		container.checkMeridian();
 	}
 	else {
-		container.makeWhite();
+		container.startColor();
 	}
 	})
 
@@ -148,3 +251,42 @@ var activeColumn;
 	}
 }
 
+
+
+
+// var sounds = {
+// 	containers: [
+// 	{
+// 		sound_A1 : "sounds/freeway/freeway1.ogg",
+// 		sound_A2 : "sounds/highFairy/highFairy1.ogg",
+// 		sound_A3 : "sounds/eerieSpace/eerieSpace1.ogg",
+// 		sound_A4 : "sounds/claps/2017clapOS-DG3__4.mp3"
+// 	},
+// 	{
+
+// 		sound_B1 : "sounds/freeway/freeway2.ogg",
+// 		sound_B2 : "sounds/highFairy/highFairy1.ogg",
+// 		sound_A3 : "sounds/eerieSpace/eerieSpace2.ogg",
+// 		sound_B4 : "sounds/claps/2017clapOS-DG3__8.mp3"
+// 	},
+// 	{
+// 		sound_C1 : "sounds/freeway/freeway3.ogg",
+// 		sound_C2 : "sounds/highFairy/highFairy1.ogg",
+// 		sound_A3 : "sounds/eerieSpace/eerieSpace3.ogg",
+// 		sound_C4 : "sounds/claps/2017clapOS-DG3__12.mp3"		
+// 	},
+// 	{
+// 		sound_D1 : "sounds/freeway/freeway4.ogg",
+// 		sound_D2 : "sounds/highFairy/highFairy1.ogg",
+// 		sound_A3 : "sounds/eerieSpace/eerieSpace4.ogg",
+// 		sound_D4 : "sounds/claps/2017clapOS-DG3__16.mp3"
+// 	},
+// 	{
+// 		sound_E1 : "sounds/freeway/freeway5.ogg",
+// 		sound_E2 : "sounds/highFairy/highFairy1.ogg",
+// 		sound_A3 : "sounds/eerieSpace/eerieSpace5.ogg",
+// 		sound_E4 : "sounds/claps/2017clapOS-DG3__16.mp3"
+// 	}
+// 	]
+
+// };
